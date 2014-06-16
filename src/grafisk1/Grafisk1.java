b5a9baf1b5980eb2a263b6ee48bbf86577e4bf19
jcholm <johancholmberg@hotmail.com>
2014-06-16 14:21:30 +0200

    fel

--- src/grafisk1/Grafisk1.java
+++ src/grafisk1/Grafisk1.java
@@ -5,18 +5,59 @@
  */
 
 package grafisk1;
+import static com.sun.javafx.fxml.expression.Expression.add;
+import java.awt.*;
+import java.awt.LayoutManager;
 
+import javax.swing.*;
 /**
  *
  * @author johancholmberg
  */
-public class Grafisk1 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Grafisk1 extends JFrame {
+    JButton klar;
+    JButton nyled;
+    JLabel ledtrad;
+    JTextField svar;
+    JPanel bild;
+    JPanel center;
+    
+    Grafisk1(){
+        super("test");
+        JPanel north=new JPanel();
+	add(north,BorderLayout.NORTH);
+        
+        ledtrad=new JLabel("ledtråd");
+        nyled=new JButton("Ny ledtråd");
+        north.add(ledtrad);
+        north.add(nyled);
+        
+        center= new JPanel();
+        ImageIcon image = new ImageIcon("image.jpeg");
+        center.add(image, BorderLayout.CENTER);
+        
+       
+        
+        
+        
+        JPanel south=new JPanel();
+        svar=new JTextField("Svar",10);
+        klar=new JButton("Klar");
+	add(south,BorderLayout.SOUTH);
+        
+        south.add(svar);
+        south.add(klar);
+        
+        setVisible(true);
+        setLocation(300,300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Grafisk1();
+        
     }
     
 }
